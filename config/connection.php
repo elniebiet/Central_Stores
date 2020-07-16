063d94b337cf50dc00e4ae18c204b6abb3b26ec5
Aniebiet Akpan <aniebiet.akpan@gmail.com>
2020-07-16 13:35:46 +0100

    removed files

--- config/connection.php
+++
@@ -1,4 +0,0 @@
-<!-- connect to KinFinder DB  -->
-<?php
-	$dbc = mysqli_connect('localhost', 'root', 'Galileo_123', 'central_stores') or die("Error: could not connect to central_stores".mysqli_connect_error());
-?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['central']);
-
-header("Location: index.php");
